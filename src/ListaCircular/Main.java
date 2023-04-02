3ff420a006b313bbb0c21c772fd3bcedf425eb3d
p-andrade <pedrotmajr@gmail.com>
2023-04-02 16:10:33 -0300

    Exercícios de Lista Circular do curso de Estrutura de Dados em Java da DIO

--- src/ListaCircular/Main.java
+++ src/ListaCircular/Main.java
@@ -1,2 +1,24 @@
-package ListaCircular;public class Main {
+package ListaCircular;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaCircular<String> minhaListaCircular = new ListaCircular<String>();
+
+        minhaListaCircular.add("c0");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0);
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c1");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c2");
+        minhaListaCircular.add("c3");
+        System.out.println(minhaListaCircular);
+
+        System.out.println(minhaListaCircular.get(7));
+        System.out.println(minhaListaCircular.get(323));
+
+    }
 }
